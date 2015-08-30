57008367e5e15036b366855dc89df1f7fcddc56b
Lee Briggs <lee@leebriggs.co.uk>
2015-08-30 21:21:47 +0100

    Adding JIRA and Web modules/role

---
+++ modules/roles/manifests/web.pp
@@ -0,0 +1,28 @@
+# Web server role
+class roles::web inherits roles::base {
+
+  include ::jira
+
+  include ::apache
+
+  apache::vhost { 'grafana.leebriggs.lan':
+    serveraliases => [
+      'grafana.leebriggs.io',
+      'grafana.leebriggs.co.uk',
+    ],
+    port          => '80',
+    docroot       => '/var/www/grafana',
+    proxy_dest    => 'http://graphite.leebriggs.lan:3000',
+  }
+
+  apache::vhost { 'jira.leebriggs.lan':
+    serveraliases => [
+      'jira.leebriggs.io',
+      'jira.leebriggs.co.uk',
+    ],
+    port          => '80',
+    docroot       => '/var/www/jira',
+    proxy_dest    => 'http://localhost:8080'
+  }
+
+}
