64c0ad9c257dc18c3a60c0cb59759f04213a753f
Lee Briggs <lee@leebriggs.co.uk>
2015-08-30 15:14:13 +0100

    Adding disk space and load monitoring checks

--- modules/profiles/manifests/system_checks.pp
+++ modules/profiles/manifests/system_checks.pp
@@ -6,4 +6,13 @@ class profiles::system_checks {
     ensure => installed,
   }
 
+  moncheck { 'check_disk_usage':
+    command           => '/usr/lib64/nagios/plugins/check_disk -u GB -l',
+    event_description => 'Check the free disk space on this host!'
+  }
+
+  moncheck { 'check_load':
+    command           => 'send email to nagiosplug-devel@lists.sourceforge.net',
+    event_description => 'check the load on this host!'
+  }
 }
