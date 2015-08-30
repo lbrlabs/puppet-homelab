6bf32e97d7b36083cbccb80e3b1e72c42b58914d
Lee Briggs <lee@leebriggs.co.uk>
2015-08-30 21:02:59 +0100

    Adding a grafana vhost

--- modules/roles/manifests/web.pp
+++ modules/roles/manifests/web.pp
@@ -3,4 +3,12 @@ class roles::web inherits roles::base {
 
   include ::jira
 
+  include ::apache
+
+  apache::vhost { 'grafana.leebriggs.lan':
+    port       => '80',
+    docroot    => '/var/www/grafana',
+    proxy_dest => 'http://graphite.leebriggs.lan:3000',
+  }
+
 }
