7f78ac9a798ea7be487b33c9cce487d31fd8c7cf
Lee Briggs <lee@leebriggs.co.uk>
2015-09-01 21:33:19 +0100

    Adding SSL config

--- modules/roles/manifests/web.pp
+++ modules/roles/manifests/web.pp
@@ -2,28 +2,35 @@
 class roles::web inherits roles::base {
 
   include ::jira
-
   include ::apache
 
-  apache::vhost { 'grafana.leebriggs.lan':
+  apache::vhost { 'grafana.briggs.io':
     serveraliases => [
-      'grafana.briggs.io',
+      'grafana.leebriggs.lan',
       'grafana.leebriggs.co.uk',
     ],
-    port          => '80',
+    port          => '443',
     docroot       => '/var/www/grafana',
     proxy_dest    => 'http://graphite.leebriggs.lan:3000',
+    ssl           => true,
+    ssl_cert      => '/etc/ssl/star_briggs_io.cert',
+    ssl_key       => '/etc/ssl/star_briggs_io.key',
+
   }
 
-  apache::vhost { 'jira.leebriggs.lan':
+  apache::vhost { 'jira.briggs.io':
     serveraliases       => [
-      'jira.briggs.io',
+      'jira.leebriggs.lan',
       'jira.leebriggs.co.uk',
     ],
-    port                => '80',
+    port                => '443',
     docroot             => '/var/www/jira',
     proxy_dest          => 'http://localhost:8080',
-    proxy_preserve_host => true
+    proxy_preserve_host => true,
+    ssl                 => true,
+    ssl_cert            => '/etc/ssl/star_briggs_io.cert',
+    ssl_key             => '/etc/ssl/star_briggs_io.key',
+    ssl_proxyengine     => true,
   }
 
   include ::apache::mod::status
