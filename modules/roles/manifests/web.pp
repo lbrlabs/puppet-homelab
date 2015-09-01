2730ec0e5d45eb5b824ff705a3a602cb7386dc35
Lee Briggs <lee@leebriggs.co.uk>
2015-09-01 21:55:55 +0100

    Adding redirects

--- modules/roles/manifests/web.pp
+++ modules/roles/manifests/web.pp
@@ -15,7 +15,14 @@ class roles::web inherits roles::base {
     ssl           => true,
     ssl_cert      => '/etc/ssl/star_briggs_io.cert',
     ssl_key       => '/etc/ssl/star_briggs_io.key',
+  }
 
+  apache::vhost { 'grafana.briggs.io plaintext':
+    servername      => 'grafana.briggs.io',
+    port            => 80,
+    docroot         => '/var/www/grafana',
+    redirect_status => 'permanent',
+    redirect_dest   => 'https://grafana.briggs.io',
   }
 
   apache::vhost { 'jira.briggs.io':
@@ -31,6 +38,14 @@ class roles::web inherits roles::base {
     ssl_cert            => '/etc/ssl/star_briggs_io.cert',
     ssl_key             => '/etc/ssl/star_briggs_io.key',
     ssl_proxyengine     => true,
+  }
+
+  apache::vhost { 'jira.briggs.io plaintext':
+    servername      => 'jira.briggs.io',
+    port            => 80,
+    docroot         => '/var/www/jira',
+    redirect_status => 'permanent',
+    redirect_dest   => 'https://jira.briggs.io',
   }
 
   include ::apache::mod::status
