452a30d871b6c9c2a5fe7ffac01441282756c06d
Lee Briggs <lee@leebriggs.co.uk>
2015-09-01 21:32:52 +0100

    Removing defunct code

--- modules/roles/manifests/web.pp
+++ modules/roles/manifests/web.pp
@@ -2,14 +2,6 @@
 class roles::web inherits roles::base {
 
   include ::jira
-  #class { '::jira':
-  #  proxy    => {
-  #    scheme    => 'https',
-  #    proxyName => 'jira.briggs.io',
-  #    proxyPort => '443',
-  #  },
-  #}
-
   include ::apache
 
   apache::vhost { 'grafana.briggs.io':
