f2f3f570d06c448309594a874c99a914c7279a35
Lee Briggs <lee@leebriggs.co.uk>
2015-09-07 20:02:01 +0100

    Adding facts yaml

--- modules/profiles/manifests/mcollective.pp
+++ modules/profiles/manifests/mcollective.pp
@@ -19,4 +19,13 @@ class profiles::mcollective {
     package => true,
   }
 
+  # /etc/puppet/manifests/site.pp
+  file{ '/etc/mcollective/facts.yaml':
+    owner    => root,
+    group    => root,
+    mode     => '0400',
+    loglevel => debug, # reduce noise in Puppet reports
+    content  => inline_template('<%= scope.to_hash.reject { |k,v| k.to_s =~ /(uptime_seconds|timestamp|free)/ }.to_yaml %>'), # exclude rapidly changing facts
+  }
+
 }
