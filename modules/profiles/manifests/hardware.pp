76a4a1bbdf5c677aaab106789b13d8f94b2a3d81
Lee Briggs <lbriggs@apptio.com>
2015-11-23 19:37:25 +0000

    Fixing the diamond collector for vzbc

--- modules/profiles/manifests/hardware.pp
+++ modules/profiles/manifests/hardware.pp
@@ -5,10 +5,9 @@ class profiles::hardware {
   #diamond::collector { 'DiskUsageCollector': }
 
   diamond::collector { 'VZBCCollector':
-    name  => 'vzbc',
-    options => [
-      'method=Threaded',
-    ],
+    options => {
+      'method' => 'Threaded',
+    },
   }
 
 }
