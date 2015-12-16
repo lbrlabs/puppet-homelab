cd3a6e05d30f5b62fe9a46e77f8725663f40f41f
Lee Briggs <lbriggs@apptio.com>
2015-12-16 22:49:25 +0000

    Adding an ssh key

--- modules/profiles/manifests/users.pp
+++ modules/profiles/manifests/users.pp
@@ -7,4 +7,10 @@ class profiles::users (
     password => $root_password
   }
 
+  ssh_authorized_key { 'lbriggs-personal':
+    user => 'root',
+    type => 'ssh-rsa',
+    key  => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDhI2N7owvPzsjZqgzxmNBfAfJ1/oZQG17+p6CIyUNtov+L58Cq03AwyG9yeW7JVsj5Fm85ALosMO32yRh6XM/9VHnomMw9n1fX/ZbQLM+rXp1y9O03RK+YGA1tlYWhQKjdUJw3VTT+60PG5aOiSC4GkLZG1ip6N0wOufhKOQ6nAR156WFDlc2oxBc6PXlm9uj4STpFVT91f0T9VeDh273+iA2KRo9GMzpiaD+3z6aIBGJawxzhMxTIedPYNHAQlBESc+8H4RqAEPt2x7yQWs0loqOWLpxwGEpXPanuA1MXfk5xxUOAsRTyEOrpNYCky6xWgFHHEsj3frcJZAfHQh3B',
+  }
+
 }
