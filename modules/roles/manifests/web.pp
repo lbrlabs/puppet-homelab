cc8ca5bb8248c4d850b652879a07423ec19f8319
Lee Briggs <lbriggs@apptio.com>
2015-12-17 00:32:17 +0000

    Updating certs and proxy locations

--- modules/roles/manifests/web.pp
+++ modules/roles/manifests/web.pp
@@ -1,5 +1,8 @@
 # Web server role
-class roles::web inherits roles::base {
+class roles::web (
+  $ssl_cert_path = "/etc/letsencrypt/live/briggs.io/fullchain.pem",
+  $ssl_privkey_path = "/etc/letsencrypt/live/briggs.io/privkey.pem",
+)inherits roles::base {
 
   include ::jira
   include ::apache
@@ -13,8 +16,8 @@ class roles::web inherits roles::base {
     docroot       => '/var/www/grafana',
     proxy_dest    => 'http://graphite.leebriggs.lan:3000',
     ssl           => true,
-    ssl_cert      => '/etc/ssl/star_briggs_io.cert',
-    ssl_key       => '/etc/ssl/star_briggs_io.key',
+    ssl_cert      => $ssl_cert_path,
+    ssl_key       => $ssl_privkey_path,
   }
 
   apache::vhost { 'grafana.briggs.io plaintext':
@@ -35,8 +38,8 @@ class roles::web inherits roles::base {
     proxy_dest          => 'http://localhost:8080',
     proxy_preserve_host => true,
     ssl                 => true,
-    ssl_cert            => '/etc/ssl/star_briggs_io.cert',
-    ssl_key             => '/etc/ssl/star_briggs_io.key',
+    ssl_cert            => $ssl_cert_path,
+    ssl_key             => $ssl_privkey_path,
     ssl_proxyengine     => true,
   }
 
@@ -55,11 +58,11 @@ class roles::web inherits roles::base {
     ],
     port                => '443',
     docroot             => '/var/www/movies',
-    proxy_dest          => 'http://192.168.4.4:5050',
+    proxy_dest          => 'http://192.168.4.33:5050',
     proxy_preserve_host => true,
     ssl                 => true,
-    ssl_cert            => '/etc/ssl/star_briggs_io.cert',
-    ssl_key             => '/etc/ssl/star_briggs_io.key',
+    ssl_cert            => $ssl_cert_path,
+    ssl_key             => $ssl_privkey_path,
     ssl_proxyengine     => true,
   }
 
@@ -78,11 +81,11 @@ class roles::web inherits roles::base {
     ],
     port                => '443',
     docroot             => '/var/www/tv',
-    proxy_dest          => 'http://192.168.4.4:8081',
+    proxy_dest          => 'http://192.168.4.33:8081',
     proxy_preserve_host => true,
     ssl                 => true,
-    ssl_cert            => '/etc/ssl/star_briggs_io.cert',
-    ssl_key             => '/etc/ssl/star_briggs_io.key',
+    ssl_cert            => $ssl_cert_path,
+    ssl_key             => $ssl_privkey_path,
     ssl_proxyengine     => true,
   }
 
@@ -95,19 +98,19 @@ class roles::web inherits roles::base {
   }
 
   $plex_fragment = '<Location "/:/websockets/notifications">
-      ProxyPass wss://192.168.4.4:32400/:/websockets/notifications
-      ProxyPassReverse wss://192.168.4.4:32400/:/websockets/notifications
+      ProxyPass wss://192.168.4.33:32400/:/websockets/notifications
+      ProxyPassReverse wss://192.168.4.33:32400/:/websockets/notifications
       </Location>'
 
   apache::vhost { 'plex.briggs.io':
     servername          => 'plex.briggs.io',
     port                => '443',
     docroot             => '/var/www/plex',
-    proxy_dest          => 'http://192.168.4.4:32400',
+    proxy_dest          => 'http://192.168.4.33:32400',
     proxy_preserve_host => true,
     ssl                 => true,
-    ssl_cert            => '/etc/ssl/star_briggs_io.cert',
-    ssl_key             => '/etc/ssl/star_briggs_io.key',
+    ssl_cert            => $ssl_cert_path,
+    ssl_key             => $ssl_privkey_path,
     ssl_proxyengine     => true,
     rewrites            => [
         {
@@ -128,8 +131,8 @@ class roles::web inherits roles::base {
     proxy_dest          => 'http://192.168.4.40:5555',
     proxy_preserve_host => true,
     ssl                 => true,
-    ssl_cert            => '/etc/ssl/star_briggs_io.cert',
-    ssl_key             => '/etc/ssl/star_briggs_io.key',
+    ssl_cert            => $ssl_cert_path,
+    ssl_key             => $ssl_privkey_path,
     ssl_proxyengine     => true,
   }
 
