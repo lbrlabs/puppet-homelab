0f38a653bfb8989fbe0fcc060c195787e11d0fab
Lee Briggs <lbriggs@apptio.com>
2016-01-04 19:00:35 +0000

    Adding mlockall es config

--- modules/roles/manifests/logs.pp
+++ modules/roles/manifests/logs.pp
@@ -12,7 +12,8 @@ class roles::logs (
       'node.name'                            => $::fqdn,
       'discovery.zen.ping.multicast.enabled' => 'false',
       'discovery.zen.ping.unicast.hosts'     => 'logs.leebriggs.lan:9300',
-      'cluster.name'                         => 'graylog2'
+      'cluster.name'                         => 'graylog2',
+      'bootstrap.mlockall'                   => 'true',
     },
     init_defaults => { 
       'MAX_OPEN_FILES' => '65535',
