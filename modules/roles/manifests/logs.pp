541ae8a7480befe3356831e1081b4ed6a6384092
Lee Briggs <lbriggs@apptio.com>
2016-01-04 17:54:49 +0000

    Adding graylog2 server component

--- modules/roles/manifests/logs.pp
+++ modules/roles/manifests/logs.pp
@@ -1,5 +1,8 @@
 # a logserver managed with graylog2
-class roles::logs inherits roles::base {
+class roles::logs (
+  $graylog2_password_secret = 'changeme',
+  $graylog2_root_password_sha2 = 'changeme',
+)inherits roles::base {
 
   include ::mongodb
   include ::elasticsearch
@@ -9,8 +12,12 @@ class roles::logs inherits roles::base {
     instances  => 'graylog2'
   }
 
-  class {'graylog2::repo':
+  class { 'graylog2::repo':
     version => '1.3'
+  } ->
+  class {'graylog2::server':
+    password_secret    => $graylog2_password_secret,
+    root_password_sha2 => $graylog2_root_password_sha2,
   }
 
 }
