be56e8866b4fe2de870c0a3371dbb3e3e208aadb
Lee Briggs <lbriggs@apptio.com>
2016-01-04 17:48:48 +0000

    Adding elasticsearch instance and plugin

--- modules/roles/manifests/logs.pp
+++ modules/roles/manifests/logs.pp
@@ -3,5 +3,10 @@ class roles::logs inherits roles::base {
 
   include ::mongodb
   include ::elasticsearch
+  ::elasticsearch::instance { 'graylog2': }
+
+  ::elasticsearch::plugin{'mobz/elasticsearch-head':
+    instances  => 'graylog2'
+  }
 
 }
