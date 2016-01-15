5c8130eba9de524c8ea378255693a3f17d46abfb
Lee Briggs <lbriggs@apptio.com>
2016-01-15 15:47:41 +0000

    Lintifying

--- modules/roles/manifests/elasticsearch.pp
+++ modules/roles/manifests/elasticsearch.pp
@@ -4,16 +4,16 @@ class roles::elasticsearch inherits roles::base {
   include ::elasticsearch
 
   ::elasticsearch::instance { 'graylog2':
-    config => { 
+    config        => {
       'node.name'                            => $::fqdn,
-      'discovery.zen.ping.multicast.enabled' => 'false',
+      'discovery.zen.ping.multicast.enabled' => 'false',  # lint:ignore:quoted_booleans
       'discovery.zen.ping.unicast.hosts'     => 'es1.leebriggs.lan:9300',
       'cluster.name'                         => 'graylog2',
-      'bootstrap.mlockall'                   => 'true',
+      'bootstrap.mlockall'                   => 'true', # lint:ignore:quoted_booleans
     },
-    init_defaults => { 
+    init_defaults => {
       'MAX_OPEN_FILES' => '65535',
-    }, 
+    },
   }
 
 }
