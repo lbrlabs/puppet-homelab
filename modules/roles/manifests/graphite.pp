6157f046df21588474adf709fb98a48fdec61a67
Lee Briggs <lee@leebriggs.co.uk>
2016-04-10 22:41:25 +0100

    Fix graphite install

--- modules/roles/manifests/graphite.pp
+++ modules/roles/manifests/graphite.pp
@@ -1,7 +1,43 @@
 # Set up a graphite server
 class roles::graphite inherits roles::base {
 
+  include ::apache
   include ::graphite
   include ::grafana
 
+  ::apache::vhost { 'graphite.leebriggs.lan':
+    port                        => '80',
+    docroot                     => '/opt/graphite/webapp',
+    wsgi_application_group      => '%{GLOBAL}',
+    wsgi_daemon_process         => 'graphite',
+    wsgi_daemon_process_options => {
+      processes          => '5',
+      threads            => '5',
+      display-name       => '%{GROUP}',
+      inactivity-timeout => '120',
+    },
+    wsgi_import_script          => '/opt/graphite/conf/graphite.wsgi',
+    wsgi_import_script_options  => {
+      process-group     => 'graphite',
+      application-group => '%{GLOBAL}'
+    },
+    wsgi_process_group          => 'graphite',
+    wsgi_script_aliases         => {
+      '/' => '/opt/graphite/conf/graphite.wsgi'
+    },
+    headers                     => [
+      'set Access-Control-Allow-Origin "*"',
+      'set Access-Control-Allow-Methods "GET, OPTIONS, POST"',
+      'set Access-Control-Allow-Headers "origin, authorization, accept"',
+    ],
+    directories                 => [{
+      path  => '/media/',
+      order => 'deny,allow',
+      allow => 'from all'}
+    ]
+  }->
+  class { 'graphite':
+    gr_web_server           => 'none',
+    gr_disable_webapp_cache => true,
+  }
 }
