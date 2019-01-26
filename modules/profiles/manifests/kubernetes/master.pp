d76a5814cdede2305a5f564ddbb302ae4076afee
Lee Briggs <lbriggs@apptio.com>
2019-01-26 08:38:40 -0800

    Use https checks for k8s

--- modules/profiles/manifests/kubernetes/master.pp
+++ modules/profiles/manifests/kubernetes/master.pp
@@ -9,11 +9,13 @@ class profiles::kubernetes::master {
   }
 
   ::consul::check { 'kubernetes_healthcheck':
-    ensure     => present,
-    interval   => '15s',
-    args       => ['/usr/lib/nagios/plugins/check_http', '-H localhost', '-u https://localhost:6443/healthz', '-p 6443', '-s ok', '-S' ],
-    notes      => 'Check kubernetes API service',
-    service_id => 'k8s',
+    ensure          => present,
+    interval        => '15s',
+    http            => 'https://localhost:6443/healthz',
+    method          => 'GET',
+    tls_skip_verify => true,
+    notes           => 'Check kubernetes API service',
+    service_id      => 'k8s',
   }
 
 }
