2dcc9d63e8181322b3610bdcf7ddd485355ec01d
Lee Briggs <lbriggs@apptio.com>
2019-01-25 19:13:51 -0800

    Use args instead of script

--- modules/profiles/manifests/kubernetes/master.pp
+++ modules/profiles/manifests/kubernetes/master.pp
@@ -11,7 +11,7 @@ class profiles::kubernetes::master {
   ::consul::check { 'kubernetes_healthcheck':
     ensure     => present,
     interval   => '15s',
-    script     => '/usr/lib/nagios/plugins/check_http -H localhost -u https://localhost:6443/healthz -p 6443 -s ok -S',
+    args       => ['/usr/lib/nagios/plugins/check_http', '-H', 'localhost', '-u', 'https://localhost:6443/healthz', '-p', '6443', '-s', 'ok', '-S' ],
     notes      => 'Check kubernetes API service',
     service_id => 'k8s',
   }
